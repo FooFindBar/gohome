eb63cc2f968536905e9714a4576fe641a6dbd88a
FooFindBar <3205933776@qq.com>
2020-06-03 11:27:03 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="all">
+    <div class="big">
+      <div class="left">当前日期：</div>
+      <div class="right">{{datenow}}</div>
+    </div>
+    <div class="big">
+      <div class="left">距离下班时间还有：</div>
+      <div class="right">{{outnow}}</div>
+    </div>
+    <div class="big">
+      <div class="left">距离下班时间还有：</div>
+      <div class="right">{{outhh}}小时</div>
+    </div>
+    <div class="big">
+      <div class="left">距离下班时间还有：</div>
+      <div class="right">{{outmm}}分钟</div>
+    </div>
+    <div class="big">
+      <div class="left">距离下班时间还有：</div>
+      <div class="right">{{outss}}秒</div>
+    </div>
+    <div class="big">
+      <div class="left">距离下班时间还有：</div>
+      <div class="right">{{outtime}}毫秒</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        datenow: '',
+        outnow: '',
+        year: '',
+        month: '',
+        date: '',
+        hour: '',
+        minute: '',
+        second: '',
+        milliSeconds: '',
+        outtime: 0,
+        outss: 0,
+        outhh: 0,
+        outmm: 0,
+
+      }
+    },
+    created() {
+      this.timer()
+    },
+    methods: {
+      // 这是获取数据的函数
+      getData() {
+        var date = new Date();
+        this.year = date.getFullYear();
+        this.month = date.getMonth() + 1;
+        this.date = date.getDate();
+        this.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+        this.minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+        this.second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+        this.milliSeconds = date.getMilliseconds();
+        this.datenow = this.year + '-' + this.month + '-' + this.date + ' ' + this.hour + ':' + this.minute + ':' + this.second + '.' + this.milliSeconds;
+        if (this.hour >= 18 && this.hour < 9) return false
+        var a = (this.hour * 3600000) + (this.minute * 60000) + (this.second * 1000) + this.milliSeconds
+        this.outtime = 64800000 - a
+        this.outss = (this.outtime / 1000).toFixed(4)
+        this.outmm = (this.outtime / 1000 / 60).toFixed(4)
+        this.outhh = (this.outtime / 1000 / 60 / 60).toFixed(4)
+        this.outnow = this.formatDuring(this.outtime)
+      },
+      // 这是一个定时器
+      timer() {
+        return setInterval(() => {
+          this.getData()
+        }, 100)
+      },
+      formatDuring(mss) {
+        var days = parseInt(mss / (1000 * 60 * 60 * 24));
+        var hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = (mss % (1000 * 60)) / 1000;
+        return hours + " 小时 " + minutes + " 分钟 " + seconds + " 秒 ";
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+
+<style>
+  body,
+  html {
+    height: 100%;
+  }
+
+  .all {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .big {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .left {
+    padding: 5px;
+    margin-top: 25px;
+    font-size: 20px;
+    font-family: "黑体";
+    text-align: end;
+    width: 200px;
+  }
+
+  .right {
+    text-align: start;
+    width: 300px;
+    border: 3px solid white;
+    padding: 5px;
+    margin-top: 20px;
+    font-size: 20px;
+    font-family: "黑体";
+    box-shadow: 5px 5px 10px #666666, inset 5px 5px 10px #666666;
+    border-radius: 5px;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    runtimeCompiler: true,
+    publicPath: './',  //输出的根路径  默认是/ 如果你的网站是app.com/vue 这更改此配置项
+    devServer: {
+      port: 8888 //端口
+    }
+  }
